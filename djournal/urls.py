bec8d000e1416ff8779d3f5f7c0cfb743b751955
Reniel Ocampo <renielxocampo@gmail.com>
2021-10-17 15:43:18 -0700

    add articles app

--- djournal/urls.py
+++ djournal/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^articles/$', include('articles.urls')),
     re_path(r'^about/$', views.about),
     re_path(r'^$', views.home),
 ]
