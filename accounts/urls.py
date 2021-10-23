a2be500e3fafeb9f55c948172954980f0f2aeb1d
Reniel Ocampo <renielxocampo@gmail.com>
2021-10-23 16:06:03 -0700

    add login view

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ app_name = 'accounts'
 
 urlpatterns = [
   re_path(r'^signup/$', views.signup_view, name="signup"),
+  re_path(r'^login/$', views.login_view, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 def signup_view(request):
   if request.method == 'POST':
@@ -14,3 +14,16 @@ def signup_view(request):
     form = UserCreationForm()
   
   return render(request, 'accounts/signup.html', { 'form': form })
+
+def login_view(request):
+  if request.method == 'POST':
+    form = AuthenticationForm(data=request.POST)
+
+    if form.is_valid():
+      # TODO: log the user in
+
+      return redirect('articles:list')
+  else:
+    form = AuthenticationForm()
+
+  return render(request, 'accounts/login.html', { 'form': form })
